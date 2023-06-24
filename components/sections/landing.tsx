5997137be6ed47118f16f95ffaa99f43a92bcb77
Robspin <robin@tracefy.com>
2023-06-24 13:50:17 +0200

    initial

---
+++ components/sections/landing.tsx
@@ -0,0 +1,12 @@
+
+export default function Landing() {
+    return (
+        <section className="w-full h-screen flex justify-center items-center">
+            <div className="mx-auto max-w-2xl text-center">
+                <div><h1 className="text-4xl font-bold tracking-tight sm:text-6xl">We solve problems.<br/>So you don't have to.</h1><p
+                    className="mt-6 text-lg leading-8">Specialists in IT solutions from dev-ops to frontend</p>
+                </div>
+            </div>
+        </section>
+    )
+}
