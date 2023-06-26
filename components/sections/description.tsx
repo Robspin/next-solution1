a29c3a131f84f2b9e619a031c69820d571df668f
Robspin <robin@tracefy.com>
2023-06-26 20:52:33 +0200

    basic animations

--- components/sections/description.tsx
+++ components/sections/description.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+"use client"
+import Image from "next/image"
+import { motion } from "framer-motion"
 
 const Lightning = ({ ...props }) => (<svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
@@ -14,7 +16,12 @@ const Sliders = ({ ...props }) => (<svg {...props} xmlns="http://www.w3.org/2000
 export default function Description() {
     return (
         <section className="w-full">
-            <div className="max-md:flex max-md:flex-col md:grid md:grid-cols-2 w-full gap-20 md:p-8">
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ type: "spring", bounce: 0.2, duration: 1.4, delay: 0.2 }}
+                viewport={{ once: true, amount: 0.4 }}
+                className="max-md:flex max-md:flex-col md:grid md:grid-cols-2 w-full gap-20 md:p-8">
                 <div className="rounded h-[60vh] w-full relative overflow-hidden">
                     <Image src="/assets/description-1.jpg" alt="description" className="object-cover" fill={true} />
                 </div>
@@ -25,21 +32,26 @@ export default function Description() {
                         <p>We are not afraid to put new technologies to the test. Our passion is researching new ways to provide faster, robust and powerful solutions.</p>
                     </div>
                 </div>
-            </div>
-            <div className="w-full h-[10vh]" />
-            <div className="max-md:flex max-md:flex-col-reverse md:grid md:grid-cols-2 w-full gap-8 md:p-8">
+            </motion.div>
+            <div className="w-full h-[20vh]" />
+            <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ type: "spring", bounce: 0.2, duration: 1.4, delay: 0.2 }}
+                viewport={{ once: true, amount: 0.4 }}
+                className="max-md:flex max-md:flex-col-reverse md:grid md:grid-cols-2 w-full gap-8 md:p-8">
                 <div className="w-full h-full max-md:mt-8">
                     <div className="w-full h-full flex flex-col justify-center">
                         <div className="mb-4 text-tint-secondary dark:text-tint flex font-medium"><Sliders className="w-6 h-6 mr-2"/> CUSTOMIZED AND COLLABORATIVE</div>
                         <h3 className="text-3xl font-extrabold tracking-tight mb-4">One size doesn't fit all</h3>
-                        <p>Our team of experts at Solution One is adept at understanding your needs and tailoring solutions specifically for your use-case.
+                        <p>Our team at Solution 1 is adept at understanding your needs and tailoring solutions specifically for your use-case.
                         </p>
                     </div>
                 </div>
                 <div className="rounded h-[60vh] w-full relative overflow-hidden">
                     <Image src="/assets/description-2.jpg" alt="description" className="object-cover" fill={true} />
                 </div>
-            </div>
+            </motion.div>
         </section>
     )
 }
--- components/sections/landing.tsx
+++ components/sections/landing.tsx
@@ -1,16 +1,46 @@
+"use client"
+import { useState } from "react"
 import { inter } from "@/lib/fonts"
+import { AnimatePresence, motion, useMotionValueEvent, useScroll } from "framer-motion"
+
 
 export default function Landing() {
+    const [showScrollIcon, setShowScrollIcon] = useState(true)
+    const { scrollY } = useScroll()
+
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        if (latest === 0) setShowScrollIcon(true)
+        else if (latest !== 0 && showScrollIcon) setShowScrollIcon(false)
+    })
+
     return (
         <section className="w-full h-screen flex justify-center items-center relative">
             <div className="mx-auto max-w-2xl text-center">
-                <div><h1 className={`text-4xl font-bold tracking-tight sm:text-6xl ${inter.className}`}>We solve problems.<br/>So you don't have to.</h1><p
-                    className="mt-6 text-lg leading-8">Specialists in IT solutions from dev-ops to frontend</p>
+                <div>
+                    <motion.h1
+                        initial={{ opacity: 0, x: "-15vw" }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.90, delay: 0.5 }}
+                        className={`text-4xl font-bold tracking-tight sm:text-6xl ${inter.className}`}>We solve problems.</motion.h1>
+                    <motion.h1
+                        initial={{ opacity: 0, x: "15vw" }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.90, delay: 0.5 }}
+                        className={`text-4xl font-bold tracking-tight sm:text-6xl ${inter.className}`}>So you don't have to.</motion.h1>
+
+                    <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 1.40 }} className="mt-6 text-lg leading-8">Specialists in IT solutions from dev-ops to frontend</motion.p>
                 </div>
             </div>
-            <div className="absolute bottom-10 h-10 w-6 border-2 border-gray-300 rounded-xl flex justify-center">
-                <div className="border border-gray-300 h-2 mt-2 rounded animate-scroll"/>
-            </div>
+            <AnimatePresence>
+                {showScrollIcon && <motion.div
+                    initial={{ opacity: 0, y: 5 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8, delay: 0.3 }}
+                    exit={{ opacity: 0, y: 5 }}
+                    className="absolute bottom-10 h-10 w-6 border-2 border-gray-300 rounded-xl flex justify-center">
+                    <div className="border border-gray-300 h-2 mt-2 rounded animate-scroll"/>
+                </motion.div>}
+            </AnimatePresence>
         </section>
     )
 }
