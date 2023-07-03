d6e1506b1364d8b46642ae9c7e04058b39db3622
Robspin <robin@tracefy.com>
2023-07-03 07:41:51 +0200

    hotfix mobile navbar bg

--- components/layout/mobile-menu.tsx
+++ components/layout/mobile-menu.tsx
@@ -3,7 +3,7 @@ import { Fragment, ReactNode, useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import Link from "next/link"
 import { Solution1 } from "@/components/layout/footer"
-import ThemeSwitch from "@/components/theme-switch";
+import ThemeSwitch from "@/components/theme-switch"
 
 const Burger = ({ ...props }) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
@@ -15,7 +15,6 @@ const Cross = ({ ...props }) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
-
 )
 
 const Transition = ({ children, className, ...props }: { children: ReactNode, className?: string }) => {
@@ -49,7 +48,7 @@ export default function MobileMenu() {
             <AnimatePresence>
                 {menuOpened && <Transition
                     key="menu"
-                    className="top-0 bottom-0 right-0 left-0 h-full fixed bg-black dark:bg-white inline md:hidden overflow-hidden">
+                    className="top-0 bottom-0 right-0 left-0 h-full fixed bg-white dark:bg-black inline md:hidden overflow-hidden">
                         <div className="w-screen h-full fixed flex flex-col justify-between items-center p-3">
                             <button aria-label="close-navigation-menu" className="inline md:hidden ml-auto" onClick={toggleMenu}>
                                 <Cross className="w-8 h-8" />
