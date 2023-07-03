e84af8bdfa64619c5b9e1178069702213dc9998e
Robspin <robin@tracefy.com>
2023-07-03 19:41:19 +0200

    priority image prop

--- components/layout/page-container.tsx
+++ components/layout/page-container.tsx
@@ -6,7 +6,7 @@ export default function PageContainer({ children, background = false }: { childr
     return (
         <main className="w-screen flex flex-col items-center relative">
             {background && <div className="h-[70vh] w-[80vh] absolute pointer-none right-0">
-                <Image src="/assets/hero-gradient.svg" alt="gradient background" className="object-cover" fill={true}/>
+                <Image src="/assets/hero-gradient.svg" alt="gradient background" className="object-cover" priority fill={true}/>
             </div>}
             <div className="xl:min-w-[1280px] max-w-[1280px] px-4">
                 {children}
